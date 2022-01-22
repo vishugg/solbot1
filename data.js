d7adfe8db196cb4aa210092a6a6b0dabd5fd8107
vishugg <88967649+vishugg@users.noreply.github.com>
2022-01-22 12:21:35 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,36 @@
+module.exports = {
+
+mongoLink:'mongodb+srv://nft:vishu@paid1.qldpv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',//database link
+
+bot_token:'5149189289:AAEu78GYvYpX30wnnLzyCmRasZo24V8yBfk',//bot token
+
+bot_name:'InstantSolAirdrop_bot',//bot username without @
+
+bot_admin:1891485685,// admin telegram id
+
+channelsList : ['@AirdropBen' , '@OfficialMrAirdrops' , '@TheAirdropX' , '@Musk_Signal','@genuinesairdropss','@PerfectAirdropOfficial'],// channels list
+
+channelscheck : ['@AirdropBen'],
+
+dp: '*Coming soon*',
+
+reffer_bonus:0.00015,// refferal bonus amount
+
+min_wd:0.0006,//min withdrawal amount
+
+daily_bonus:100000000000000,//,// daily bonus amount
+
+currency:'SOL', //bot cuurency
+
+key:'a1c32c2ee1261bd5cdfc1a9a6f9446d7e4586b3f64fb9bb9215509122a3fb653',
+
+secret:'e00C6f898fBb172400D4E6D7cAB67bFb4980e989D0471Deb703E87EA8D6c0287',
+
+payment_channel:'@Payout_X',// payment channel username
+
+tw1:'http://twitter.com/SK__COMMUNITY',
+
+tw2:'http://Instagram.com/MrShubzzz',
+
+
+}
